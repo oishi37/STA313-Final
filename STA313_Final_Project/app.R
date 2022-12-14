abed372bc39f6381fc157b038b46717e4ebf7af9
Eric Yin <ericyin23@gmail.com>
2022-12-13 21:12:47 -0500

    added policies and clicking

--- STA313_Final_Project/app.R
+++ STA313_Final_Project/app.R
@@ -1,12 +1,15 @@
 library(tidyverse)
+library(knitr)
 library(shinydashboard)
 library(shinyBS)
 source("helpers.R")
 data <- read.csv("finaldata_313.csv")
+federal_laws <- read.csv("federal_laws.csv")
+federal_laws <- federal_laws[-(1:4), ]
 
 ui <- fluidPage(
   
-
+  theme = bslib::bs_theme(bootswatch = "darkly"),
   
   # App title ----
   titlePanel("US Mass Shootings"),
@@ -20,9 +23,9 @@ ui <- fluidPage(
       # Input: Slider for the years of interest ----
       sliderInput(inputId = "years",
                   label = "Year of Interest:",
-                  min = 1966,
+                  min = 1960,
                   max = 2019,
-                  value= c(1966,2019)),
+                  value= c(1960,2019)),
       
       
       # Input: Input for the response type ----
@@ -38,10 +41,7 @@ ui <- fluidPage(
                     unique(data$State)))), 
                   selected = "All States",
                   multiple = TRUE),
-      
-      bsPopover("policies", "Policy Name", 
-                placement = "bottom", 
-                trigger = "hover")
+
     ),
     
     
@@ -50,11 +50,17 @@ ui <- fluidPage(
     # Main panel for displaying outputs ----
     mainPanel(
       
-      # Output: Line Graph ----
-      plotOutput(outputId = "timeline")
-      
+      # Output: Line Graph, clicks represent points ----
+      plotOutput(outputId = "timeline", click = "plot_click")
+      )
+    ), 
+  
+  fluidRow(
+    column(width = 12,
+           verbatimTextOutput("click_info")
     )
   )
+      
 )
 
 
@@ -62,30 +68,36 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output, session) {
   
+  
   output$timeline <- renderPlot({
     
-    
-    #takes input from responseID, switch function assigns values to the
-    #different inputs
-    response <- switch(input$response, 
-                       "Shootings" = "Shootings",
-                       "Fatalities" = "Fatalities",
-                       "Injuries" = "Injured")
-    
-    #takes input from state
-    if("All States" %in% input$state){
-      states <- as.character(unique(data$State))
-    } else {
-      states <- input$state
-    }
 
     
+    #creates the filtered data based on the input, returns as a list
+    args <- create_temps(input$state, input$years[2], 
+                         input$years[1], input$response)
+    
     #plots the timeline with input of the function in helpers.R
-    plot_timeline(response, input$years[1], input$years[2], states)
+    plot_timeline(args[[1]], args[[2]], input$response)
   })
   
-  addPopover(session, "timeline", "Data", content = paste0("hello")
-             , trigger = 'hover')
+  #takes the click info and finds the point in data closest to the click
+  output$click_info <- renderPrint({
+    
+    #creates the filtered data based on the input, returns as a list
+    args <- create_temps(input$state, input$years[2],
+                         input$years[1], input$response)
+
+    #nearpoints matches the click and outputs the data
+    temp_year <- nearPoints(args[[1]], input$plot_click,
+                            xvar = "Year", yvar = "count")
+    
+    #this code grabs the corresponding data and outputs it
+    law <- federal_laws %>% filter(Year_Implemented %in% temp_year[1])
+    cat(paste0("Name: ", law$Name, " [", law$Year_Implemented, 
+               "]\nDescription: ", law$Description))
+  })
+
   
 }
 
